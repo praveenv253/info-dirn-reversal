ba0e785ffdaf60f0ebbbb17ca66a0ef9b6b8e470
Praveen Venkatesh <praveenv253@gmail.com>
2015-01-24 14:48:46 -0500

    Make corrections to noisy fb dir info calcs.

--- noisy-fb-progression.py
+++ noisy-fb-progression.py
@@ -6,44 +6,50 @@ import matplotlib.pyplot as plt
 from noisy_fb_dir_info import dir_info_fwd, dir_info_rev
 
 if __name__ == '__main__':
-    n_lim = 1000 # Maximum number of iterations
+    n_lim = 100 # Maximum number of iterations
     snrs = np.array([0.25, 0.5, 1])
     sigma_n = np.ones(snrs.size)
-    sigma_r = sigma_n
     sigma_theta = snrs
+    rev_noise_ratio = 0.35
+    sigma_r = rev_noise_ratio * sigma_n
 
     blues = plt.get_cmap('Blues')
     reds = plt.get_cmap('Reds')
     greens = plt.get_cmap('Greens')
     cnums = np.logspace(1.8, 2.5, len(snrs)).astype(int)
 
-    n_vals = np.logspace(np.log2(1), np.log2(n_lim), 50, base=2).astype(int)
+    n_vals = np.logspace(np.log2(1), np.log2(n_lim), 10, base=2).astype(int)
     fwd_directed_info = np.empty((len(n_vals), len(snrs)))
     rev_directed_info = np.empty((len(n_vals), len(snrs)))
 
-    for i in range(len(n_vals)):
-        n = n_vals[i]
-        fwd_directed_info[i, :] = dir_info_fwd(sigma_theta**2, sigma_n**2,
-                                               sigma_r**2, n)
-        rev_directed_info[i, :] = dir_info_rev(sigma_theta**2, sigma_n**2,
-                                               sigma_r**2, n)
+    for j in range(len(snrs)):
+        snr = snrs[j]
+        var_n = sigma_n[j] ** 2
+        var_theta = sigma_theta[j] ** 2
+        var_r = sigma_r[j] ** 2
+
+        for i in range(len(n_vals)):
+            n = n_vals[i]
+            fwd_directed_info[i, j] = dir_info_fwd(var_theta, var_n, var_r, n)
+            rev_directed_info[i, j] = dir_info_rev(var_theta, var_n, var_r, n)
 
     for i in range(len(snrs)):
         cnum = cnums[i]
         plt.semilogx(n_vals, fwd_directed_info[:, i], color=blues(cnum))
         plt.semilogx(n_vals, rev_directed_info[:, i], color=reds(cnum))
 
-    for i in range(len(snrs)):
-        snr = snrs[i]
-        cnum = cnums[i]
-        # TODO Need to adjust the convergence criterion
-        plt.semilogx([100./snr, 100./snr], [0, 3], color=greens(cnum),
-                     linestyle='--')
+    # TODO Need to adjust the convergence criterion
+    #for i in range(len(snrs)):
+    #    snr = snrs[i]
+    #    cnum = cnums[i]
+    #    plt.semilogx([100./snr, 100./snr], [0, 3], color=greens(cnum),
+    #                 linestyle='--')
 
-    plt.xlabel(r'Iterations ($n$)')
-    plt.ylabel(r'Directed information in bits')
+    plt.xlabel(r'Iterations ($n$)', fontsize=18)
+    plt.ylabel(r'Directed information in bits', fontsize=18)
     plt.title('Comparison of directed information flows in the\nSchalkwijk and'
-              ' Kailath scheme')
+              ' Kailath scheme\n$\sigma_R^2/\sigma_N^2=%.2f$'
+              % rev_noise_ratio, fontsize=20)
     plt.legend(('$SNR=0.25$, fwd', '$SNR=0.25$, rev',
                 '$SNR=0.5$, fwd', '$SNR=0.5$, rev',
                 '$SNR=1$, fwd', '$SNR=1$, rev'), loc='upper left')
--- noisy_fb_dir_info.py
+++ noisy_fb_dir_info.py
@@ -8,11 +8,30 @@ def dir_info_fwd(var_theta, var_n, var_r, n):
 
     for i in range(1, n+1):
         if i == 1:
-            dir_info += 0.5 * np.log(1 + var_theta / var_n)
+            dir_info += 0.5 * np.log2(1 + var_theta / var_n)
+        if i == 2:
+            var_theta_theta = (var_r + var_n
+                               + var_theta * var_n / (var_theta + var_n))
+            htheta_theta = 0.5 * np.log2(var_theta_theta)
+            htheta_x_theta = 0.5 * np.log2(var_n)
+            dir_info += htheta_theta - htheta_x_theta
         else:
-            var_bar = var_theta + var_n/(i-1) + (i-2)*var_r/(i-1)**2
-            dir_info += 0.5 * np.log( (var_theta + var_r + var_n
-                                       - var_theta**2 / var_bar) / var_n )
+            # Construct a matrix for htheta_theta
+            var_uu = var_theta + var_r + var_n
+            var_uv = var_theta * np.ones((i-1, 1))
+            var_vv = np.empty((i-1, i-1))
+            for _p in range(i-1):
+                p = _p + 1
+                for _q in range(i-1):
+                    q = _q + 1
+                    var_vv[_p, _q] = (var_theta + min(p, q) * var_n / (p * q)
+                                      + min(p-1, q-1) * var_r / (p * q))
+            var_vv_inv = np.linalg.inv(var_vv)
+            var_theta_theta = var_uu - np.dot(var_uv.T,
+                                              np.dot(var_vv_inv, var_uv))
+            htheta_theta = 0.5 * np.log2(var_theta_theta)
+            htheta_x_theta = 0.5 * np.log2(var_n)
+            dir_info += htheta_theta - htheta_x_theta
 
     return dir_info
 
@@ -20,7 +39,7 @@ def dir_info_fwd(var_theta, var_n, var_r, n):
 def dir_info_rev(var_theta, var_n, var_r, n):
     dir_info = 0
 
-    for i in range(n+1):
+    for i in range(n):
         # We are leaving out the 2 * pi * e in the entropy calculations,
         # because it will get cancelled at the end anyway.
         if i == 0:
@@ -30,27 +49,28 @@ def dir_info_rev(var_theta, var_n, var_r, n):
             hx_x = 0.5 * np.log2(var_n + var_r)
             hx_x_theta = 0.5 * np.log2(var_r)
         else:
-            var_bar = var_n / (i - 1) + (i - 2) * var_r / (i - 1)**2
-            hx_x = 0.5 * np.log2( (var_r**2 + 2*var_r*var_bar
-                                   + var_n*var_r/i**2 + var_n*var_bar/i**2)
-                                  / (var_bar + var_r) )
-            # H(X_{i+1} | X^{i}, \hat\Theta^{i}):
-            var_bar_i = var_n / i + (i - 1) * var_r / i**2
-            det_sigma_22 = ( (var_r + var_bar) * (var_bar_i + var_theta)
-                             - (var_r / i - (i-1) * var_bar / i) ** 2 )
-            sigma_11 = 2 * var_r #+ var_n / i**2
-            sigma_12 = [-var_r, -var_r/i] #var_n/i - var_r/i]
-            sigma_21 = sigma_12
-            sigma_22 = [ [var_bar_i + var_theta, (i-1)*var_bar/i - var_r/i],
-                         [(i-1)*var_bar/i - var_r/i, var_r + var_bar] ]
-            conditional_var = ( sigma_11
-                                - ( sigma_12[0] * sigma_22[0][0] * sigma_21[0]
-                                  + sigma_12[0] * sigma_22[0][1] * sigma_21[1]
-                                  + sigma_12[1] * sigma_22[1][0] * sigma_21[0]
-                                  + sigma_12[1] * sigma_22[1][1] * sigma_21[1])
-                                / det_sigma_22
-                              )
-            hx_x_theta = 0.5 * np.log2(conditional_var)
+            # Construct matrix for hx_x
+            var_uu = 2 * var_r + var_n / i**2
+            var_uv = np.zeros((i, 1))
+            var_uv[-1] = - var_r
+            var_vv = np.zeros((i, i))
+            var_vv[0, 0] = var_theta
+            for _p in range(1, i):
+                p = _p + 1
+                for _q in range(1, i):
+                    q = _q + 1
+                    var_vv[_p, _q] = (min(p-2, q-2)*var_r / ((p-1) * (q-1))
+                                      + min(p-1, q-1)*var_n / ((p-1) * (q-1)))
+                    if p == q:
+                        var_vv[_p, _q] += var_r
+                    elif p > q:
+                        var_vv[_p, _q] += var_r / (p-1)
+                    else:
+                        var_vv[_p, _q] += var_r / (q-1)
+            var_vv_inv = np.linalg.inv(var_vv)
+            var_x_x = var_uu - np.dot(var_uv.T, np.dot(var_vv_inv, var_uv))
+            hx_x = 0.5 * np.log2(var_x_x)
+            hx_x_theta = 0.5 * np.log2(var_r)
 
         # Add mutual information for each `i' to get net directed information
         dir_info += hx_x - hx_x_theta
