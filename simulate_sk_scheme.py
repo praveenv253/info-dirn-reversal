fa7fba4d3324dd1b4bc9652d0dd345d8d5cf2be6
Praveen Venkatesh <praveenv253@gmail.com>
2015-05-21 15:58:23 -0400

    Add snr and rev noise ratios to simulate_sk.

--- simulate_sk_scheme.py
+++ simulate_sk_scheme.py
@@ -52,14 +52,19 @@ def simulate_sk(num_iter=100, snr=1.0, rev_noise_ratio=0, theta0=None):
 
 if __name__ == '__main__':
     num_iter = 500
-    xs, theta_hats, errors = simulate_sk(num_iter=500, snr=0.5,
-                                         rev_noise_ratio=0.1, theta0=1)
+    snr = 0.5
+    rev_noise_ratio = 0.1
+    theta0 = 1
+    xs, theta_hats, errors = simulate_sk(num_iter, snr, rev_noise_ratio,
+                                         theta0)
 
     t = np.arange(num_iter)
     plt.plot(t, xs)
-    plt.plot(t, xs[0] * np.ones(t.size), 'k--', linewidth=2)
+    plt.plot(t, theta0 * np.ones(t.size), 'k--', linewidth=2)
     plt.errorbar(t, theta_hats, errors)
-    plt.title('Evolution of $X_i$ and $\hat{\Theta}_i$\nError is 2-$\sigma$')
+    plt.title('Evolution of $X_i$ and $\hat{\Theta}_i$\nError is 2-$\sigma$\n'
+              r'$\sigma_r^2/\sigma_n^2 = %f,\,\sigma_\theta^2/\sigma_n^2 = %f$'
+              % (rev_noise_ratio ** 2, snr ** 2))
     plt.legend(('$X_i$', '$\Theta$', '$\hat{\Theta}_i$'))
     plt.xlabel('$i$')
     plt.ylabel('$X_i$ or $\hat{\Theta}_i$')
